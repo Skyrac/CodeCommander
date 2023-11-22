2b00d5770726160e502e4576daa42ce9a9b15de1
Fabian Reichenberg <fabian.reichenberg@aboat-entertainment.com>
2023-11-22 06:41:24 +0100

    Projektdateien hinzufügen.

---
+++ CSharp_ErsteSchritte/ArraysUndKollektionen/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ArraysUndKollektionen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ CSharp_ErsteSchritte/ObjektorientierteProgrammierung/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ObjektorientierteProgrammierung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
